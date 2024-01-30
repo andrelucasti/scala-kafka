55e137cce9b1727b4b9130f50d738047993a9f35
Andre Lucas <andrelucastic@gmail.com>
2024-01-30 16:24:32 +0000

    add kafka config single node

--- src/main/scala/StartApp.scala
+++ src/main/scala/StartApp.scala
@@ -1,22 +1,42 @@
 package io.andrelucas
 
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecord, RecordMetadata}
 
 import java.util.Properties
+import scala.util.Try
 
 object StartApp {
 
   private val configMap = {
     val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
+    props.put("bootstrap.servers", "localhost:29092")
+    props.put("client.id", "KafkaProducerExample")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
 
     props
   }
-
-
   val producer = new KafkaProducer[String, String](configMap)
 
-  def main(args: Array[String]): Unit = {
+  private val publishMessage = (topic: String,
+                                message: String,
+                                producer: KafkaProducer[String, String]) => {
 
+    val record = new ProducerRecord[String, String](topic, "teste", message)
+    producer.send(record, (metadata: RecordMetadata, exception: Exception) => {
+      Try {
+        println("Message sent to topic -> " + topic + " , partition -> " + metadata.partition() + " , offset -> " + metadata.offset())
+      }.getOrElse(println("Error sending message to topic -> " + topic))
+    })
+
+    producer.flush()
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    1.to(100).foreach { i =>
+      publishMessage("teste-partitions", s"Message $i", producer)
+    }
   }
 }
