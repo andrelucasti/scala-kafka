f074791777078e7cc8005a0eec439eaca3959981
Andre Lucas <andrelucastic@gmail.com>
2024-01-30 17:59:24 +0000

    add delivery channel sync and async

--- src/main/scala/StartApp.scala
+++ src/main/scala/StartApp.scala
@@ -6,37 +6,58 @@ import java.util.Properties
 import scala.util.Try
 
 object StartApp {
+  
+  val executor = java.util.concurrent.Executors.newFixedThreadPool(10)
 
-  private val configMap = {
+  private def configMap = {
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:29092")
     props.put("client.id", "KafkaProducerExample")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-
     props
   }
-  val producer = new KafkaProducer[String, String](configMap)
+  private val syncDeliveryChannel:Callback = (metadata: RecordMetadata,
+                                              exception: Exception) => {
+    if(Try(exception).isSuccess){
+      println(s"Message sent to topic -> ${metadata.topic()}, " +
+        s"partition -> ${metadata.partition()}, " +
+        s"offset -> ${metadata.offset()} " +
+        s"current thread -> ${Thread.currentThread().getName}")
 
-  private val publishMessage = (topic: String,
-                                message: String,
-                                producer: KafkaProducer[String, String]) => {
-
-    val record = new ProducerRecord[String, String](topic, "teste", message)
-    producer.send(record, (metadata: RecordMetadata, exception: Exception) => {
-      Try {
-        println("Message sent to topic -> " + topic + " , partition -> " + metadata.partition() + " , offset -> " + metadata.offset())
-      }.getOrElse(println("Error sending message to topic -> " + topic))
-    })
-
-    producer.flush()
+    } else {
+      println(s"Error while sending message ${metadata.topic()}")
+    }
   }
 
+  private val asyncDeliveryChannel: Callback = (metadata: RecordMetadata,
+                                               exception: Exception) => {
+    Thread.sleep(200)
+    def exec = executor.submit(new Runnable {
+      override def run(): Unit = {
+        println(s"Message sent to topic -> ${metadata.topic()}, " +
+          s"partition -> ${metadata.partition()}, " +
+          s"offset -> ${metadata.offset()} " +
+          s"current thread -> ${Thread.currentThread().getName}")
+      }
+    })
 
+    exec
+  }
+
+  private val publishMessage = (topic: String, message: String, producer: KafkaProducer[String, String]) => {
+    val record = new ProducerRecord[String, String](topic, message)
+    producer.send(record, syncDeliveryChannel)
+    producer.flush()
+  }
+  
   def main(args: Array[String]): Unit = {
-    1.to(100).foreach { i =>
-      publishMessage("teste-partitions", s"Message $i", producer)
+    println(s"current thread -> ${Thread.currentThread().getName}")
+    val producer = new KafkaProducer[String, String](configMap)
+    1.to(1000).foreach { i =>
+      println(s"Sending message $i -> ${Thread.currentThread().getName}")
+      publishMessage("teste-partitions", s"Messages $i", producer)
     }
   }
 }
