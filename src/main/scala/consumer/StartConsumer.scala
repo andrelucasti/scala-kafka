46f29024808a6a923865e77552636757660bf5d2
Andre Lucas <andrelucastic@gmail.com>
2024-02-03 05:30:34 +0000

    Create Consumer

---
+++ src/main/scala/consumer/StartConsumer.scala
@@ -0,0 +1,33 @@
+package io.andrelucas
+package consumer
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+
+import java.time.Duration
+import java.util.{Collections, Properties}
+
+object StartConsumer {
+
+  private def configMap = {
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:29092")
+    props.put("client.id", "scala-app-consumer")
+    props.put("group.id", "scala-group")
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+    props
+  }
+
+  def main(args: Array[String]): Unit = {
+    val consumer = KafkaConsumer[String, String](configMap)
+    consumer.subscribe(Collections.singleton("test-partitions"))
+
+    while (true){
+      val records = consumer.poll(Duration.ofMillis(200))
+      records.forEach{ r =>
+        println(r.value())
+      }
+    }
+  }
+}
--- src/main/scala/producer/StartProducer.scala
+++ src/main/scala/producer/StartProducer.scala
@@ -1,13 +1,14 @@
 package io.andrelucas
+package producer
 
 import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecord, RecordMetadata}
 
 import java.util.Properties
 import scala.util.Try
 
-object StartApp {
-  
-  val executor = java.util.concurrent.Executors.newFixedThreadPool(10)
+object StartProducer {
+
+  val executor = java.util.concurrent.Executors.newScheduledThreadPool(10)
 
   private def configMap = {
     val props = new Properties()
@@ -20,6 +21,7 @@ object StartApp {
   }
   private val syncDeliveryChannel:Callback = (metadata: RecordMetadata,
                                               exception: Exception) => {
+    Thread.sleep(200)
     if(Try(exception).isSuccess){
       println(s"Message sent to topic -> ${metadata.topic()}, " +
         s"partition -> ${metadata.partition()}, " +
@@ -33,9 +35,9 @@ object StartApp {
 
   private val asyncDeliveryChannel: Callback = (metadata: RecordMetadata,
                                                exception: Exception) => {
-    Thread.sleep(200)
     def exec = executor.submit(new Runnable {
       override def run(): Unit = {
+        Thread.sleep(200)
         println(s"Message sent to topic -> ${metadata.topic()}, " +
           s"partition -> ${metadata.partition()}, " +
           s"offset -> ${metadata.offset()} " +
@@ -44,20 +46,22 @@ object StartApp {
     })
 
     exec
+
   }
 
   private val publishMessage = (topic: String, message: String, producer: KafkaProducer[String, String]) => {
     val record = new ProducerRecord[String, String](topic, message)
-    producer.send(record, syncDeliveryChannel)
+    producer.send(record, asyncDeliveryChannel)
     producer.flush()
   }
-  
+
   def main(args: Array[String]): Unit = {
     println(s"current thread -> ${Thread.currentThread().getName}")
     val producer = new KafkaProducer[String, String](configMap)
     1.to(1000).foreach { i =>
+      Thread.sleep(200)
       println(s"Sending message $i -> ${Thread.currentThread().getName}")
-      publishMessage("teste-partitions", s"Messages $i", producer)
+      publishMessage("test-partitions", s"Messages $i", producer)
     }
   }
 }
